43e468035d7b08b65f0f4f03cca28bd18de113b3
ILyass-Lr <lirmaqui.ilyass@gmail.com>
2025-11-05 17:00:14 +0100

    Implemented PostHog

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,19 @@ const nextConfig: NextConfig = {
   },
     // Helps avoid memoization issues
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://eu-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://eu.i.posthog.com/:path*",
+      },
+    ];
+  },
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
